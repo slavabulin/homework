7ef732d222dd4398f8377569a342aa1c5c7e1c30
Aleksandr <Alex-Gnatuyk@yandex.ru>
2017-10-31 00:29:52 +0300

    Homework#1 Done

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Task1Lib;
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Writer writer = new Writer();
+           writer.Write();
+        }
+    }
+}
