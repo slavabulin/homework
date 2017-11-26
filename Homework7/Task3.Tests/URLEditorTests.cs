1902d4f8b2b01cd6e3663a0113ef6262b341fe53
slavabulin <slava-bulin@yandex.ru>
2017-11-26 21:49:34 +0300

    HW7 Task3 - small fixies

---
+++ Homework7/Task3.Tests/URLEditorTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task3;
+
+namespace Task3.Tests
+{
+    [TestClass]
+    public class URLEditorTests
+    {
+        URLEditor urlEditor;
+        public URLEditorTests()
+        {
+            urlEditor = new URLEditor();
+        }
+        [TestMethod]
+        public void AddOrChangeUrlParameterInDomainPlusKeyVal1OutDomainKeyval2()
+        {
+            //arrange
+            string inString = "www.ms.com?key1=1&key10=qwerty";
+            string keyToChange = "key1=2";
+            string retVal, expectedVal = "www.ms.com?key1=2&key10=qwerty";
+            //act
+            retVal = urlEditor.AddOrChangeUrlParameter(inString, keyToChange);
+            //assert
+            Assert.AreEqual(expectedVal, retVal);
+        }
+        [TestMethod]
+        public void AddOrChangeUrlParameterInDomainPlusKeyVal1CaseSensiriveOutDomainKeyval2()
+        {
+            //arrange
+            string inString = "www.ms.com?key1=1&key10=qwerty";
+            string keyToChange = "KEY1=2";
+            string retVal, expectedVal = "www.ms.com?key1=1&key10=qwerty&KEY1=2";
+            //act
+            retVal = urlEditor.AddOrChangeUrlParameter(inString, keyToChange);
+            //assert
+            Assert.AreEqual(expectedVal, retVal);
+        }
+        [TestMethod]
+        public void AddOrChangeUrlParameterInDomainPlusKeyValOutDomainKeyval()
+        {
+            //arrange
+            string inString = "www.ms.com";
+            string keyToChange = "KEY1=2";
+            string retVal, expectedVal = "www.ms.com?KEY1=2";
+            //act
+            retVal = urlEditor.AddOrChangeUrlParameter(inString, keyToChange);
+            //assert
+            Assert.AreEqual(expectedVal, retVal);
+        }
+        [TestMethod]
+        public void AddOrChangeUrlParameterInDomainPlusKeyVal1OutDomainKeyval1()
+        {
+            //arrange
+            string inString = "www.ms.com?key1=1&key10=qwerty";
+            string keyToChange = String.Empty;
+            string retVal, expectedVal = "www.ms.com?key1=1&key10=qwerty";
+            //act
+            retVal = urlEditor.AddOrChangeUrlParameter(inString, keyToChange);
+            //assert
+            Assert.AreEqual(expectedVal, retVal);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddOrChangeUrlParameterInUrlNullShouldThrowArgNullExcptn()
+        {
+            //arrange
+            //act
+            urlEditor.AddOrChangeUrlParameter(null, null);
+            //assert
+        }
+    }
+}
---
+++ Homework7/Task3/Task3.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    /// <summary>
+    /// You need to write a function AddOrChangeUrlParameter(url, keyValueParameter) that can manipulate URL parameters. It should be able to
+    /// add a parameter to an existing URL, but also to change a parameter if it already exists. Example:AddOrChangeUrlParameter("www.example.com",
+    /// "key=value") => 'www.example.com?key=value'(adds a parameter).AddOrChangeUrlParameter("www.example.com?key=value", "key2=value2") => 
+    /// 'www.example.com?key=value&key2=value2' (adds another parameter).AddOrChangeUrlParameter("www.example.com?key=oldValue", "key=newValue" ) => 
+    /// 'www.example.com?key=newValue'(changes the parameter).
+    /// /// </summary>
+
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class URLEditor
+    {
+        /// <summary>
+        /// Method that adds params to url if it doesnt contain it or chnges it if it does. Params assumed to be case sensetive
+        /// </summary>
+        /// <param name="url">incomming url with params</param>
+        /// <param name="keyValParam">params to add or change</param>
+        /// <returns>url with new params</returns>
+        public string AddOrChangeUrlParameter(string url, string keyValParam)
+        {
+            if (url == null) throw new ArgumentNullException(nameof(url), "argument should not be null");
+            var sb = new StringBuilder();
+            if (String.IsNullOrWhiteSpace(keyValParam)) return url;
+
+            string[] urlArr = url.Split('?');
+            string[] paramArr = keyValParam.Split('&');
+
+            string domainName = urlArr[0];
+
+            var urlParamDict = new Dictionary<string, string>();
+            var inParamDict = new Dictionary<string, string>();
+            string[] urlParamArr ;
+
+            if (urlArr.Length >1)
+            {
+                urlParamArr = urlArr[1].Split('&');
+            }
+            else
+            {
+                urlParamArr = new string[] { };
+            }
+
+            foreach(string keyValStr in urlParamArr)
+            {
+                string[] tmpStrArr = keyValStr.Split('=');
+                urlParamDict.Add(tmpStrArr.ElementAt(0), tmpStrArr.ElementAt(1));
+            }
+            foreach(string keyVal in paramArr)
+            {
+                string[] tmpStrArr = keyVal.Split('=');
+                if (urlParamDict.ContainsKey(tmpStrArr.ElementAt(0)))
+                {
+                    urlParamDict.Remove(tmpStrArr.ElementAt(0));
+                }
+                urlParamDict.Add(tmpStrArr.ElementAt(0), tmpStrArr.ElementAt(1));
+            }
+            sb.Append(domainName + "?");
+            foreach(KeyValuePair<string,string> keyVal in urlParamDict)
+            {
+                sb.Append(keyVal.Key + '=' + keyVal.Value);
+                sb.Append("&");
+            }
+            return sb.ToString().Remove(sb.ToString().Length-1);
+        }
+    }
+}
