b6df89ef25ed779c51096baa5e15770e6824411f
slavabulin <slava-bulin@yandex.ru>
2017-11-26 22:31:52 +0300

    +

---
+++ Homework9/Countdown.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Countdown.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
